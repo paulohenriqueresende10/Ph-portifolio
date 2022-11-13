8a2613ee59d9d281564bfd39f799157cea8f253a
paulohenriqueresende10 <paulo-henrique_1993@hotmail.com>
2022-11-13 12:48:50 -0300

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { MailchimpForm } from "./MailchimpForm";
+import logo from "../assets/img/logo.png";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <MailchimpForm />
+          <Col size={12} sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/paulo-henrique-santos-resende-4b9726202/"><img src={navIcon1} alt="linkedin" /></a>
+              <a href="https://www.facebook.com/paulohenrique.santosresende"><img src={navIcon2} alt="facebook" /></a>
+              <a href="https://www.instagram.com/paulohsresende/"><img src={navIcon3} alt="Instagram" /></a>
+            </div>
+            <p>Copyright 2022. Todos direitos reservados a Paulo Henrique®</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
---
+++ src/components/Projects.js
@@ -0,0 +1,75 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/receitasdavovo.png";
+import projImg2 from "../assets/img/car-shopping.png";
+import projImg3 from "../assets/img/event-plataform.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Receitas da Vovó",
+      description: "Design e desenvolvimento",
+      imgUrl: projImg1,
+      link: "https://receitasdavovo.vercel.app"
+    },
+    {
+      title: "Car Shopping",
+      description: "Design e desenvolvimento",
+      imgUrl: projImg2,
+      link: "https://car-shopping.vercel.app"
+    },
+    {
+      title: "Event Plataform",
+      description: "Desenvolvimento",
+      imgUrl: projImg3,
+      link: "https://event-plataform-beryl.vercel.app"
+    }
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projetos</h2>
+                <p>Aqui estão alguns projetos desenvolvidos em cursos de React Js</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="section">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2} alt="1" />
+    </section>
+  )
+}
